c4fd6a744b0ecb15ae0fcc497910d58b94262c82
usukhbaya12 <b20fa1714@ufe.edu.mn>
2025-05-02 03:46:53 +0800

    Facebook share 9

--- app/share/[id]/[code]/page.js
+++ app/share/[id]/[code]/page.js
@@ -11,74 +11,33 @@ export async function generateMetadata({ params }) {
   try {
     const assessmentResponse = await getAssessmentById(id);
     const testName = assessmentResponse.data?.data?.name || "Тестийн үр дүн";
-    const icon = assessmentResponse.data?.icons;
+    const icon = assessmentResponse.data?.data?.icons;
 
-    if (code) {
-      metadata = {
-        ...metadata,
+    metadata = {
+      ...metadata,
+      title: `${testName} / Hire.mn`,
+      description: `Миний "${testName}" тестийн үр дүн`,
+      openGraph: {
+        ...metadata.openGraph,
         title: `${testName} / Hire.mn`,
         description: `Миний "${testName}" тестийн үр дүн`,
-        openGraph: {
-          ...metadata.openGraph,
-          title: `${testName} / Hire.mn`,
-          description: `Миний "${testName}" тестийн үр дүн`,
-          url: `https://hire.mn/share/${id}/${code}`,
-          type: "website",
-          images: [
-            {
-              url: `https://hire.mn/api/share/${code}`,
-              width: 1600,
-              height: 837.7,
-              alt: "Test Results",
-            },
-          ],
-        },
-      };
-    } else {
-      metadata = {
-        title: `${testName} / Hire.mn`,
-        description: "Онлайн тест, хөндлөнгийн үнэлгээ",
-        openGraph: {
-          title: `${testName} / Hire.mn`,
-          description: "Онлайн тест, хөндлөнгийн үнэлгээ",
-          url: `https://hire.mn/test/${id}`,
-          siteName: "Hire.mn",
-          type: "website",
-          images: [
-            {
-              url: icon ? `${api}file/${icon}` : "https://hire.mn/misc.png",
-              width: 1200,
-              height: 630,
-            },
-          ],
-        },
-        twitter: {
-          card: "summary_large_image",
-        },
-      };
-    }
+        url: `https://hire.mn/share/${id}/${code}`,
+        type: "website",
+        images: [
+          {
+            url: `https://hire.mn/api/share/${code}`,
+            width: 1600,
+            height: 837.7,
+            alt: "Test Results",
+          },
+        ],
+      },
+    };
   } catch (error) {
     console.error("Error generating share metadata:", error);
   }
 
   return metadata;
-
-  //   return {
-  //     title: `${testName} / Hire.mn`,
-  //     openGraph: {
-  //       title: `${testName} / Hire.mn`,
-  //       description: `Миний "${testName}" тестийн үр дүн`,
-  //       type: "website",
-  //       images: [
-  //         {
-  //           url: `https://hire.mn/api/share/${code}`,
-  //           width: 1600,
-  //           height: 837.7,
-  //         },
-  //       ],
-  //       url: `https://hire.mn/share/${id}/${code}`,
-  //     },
-  //   };
 }
 
 export default async function SharePage({ params }) {
--- app/test/[id]/metadata.js
+++ app/test/[id]/metadata.js
@@ -6,59 +6,35 @@ export async function generateTestMetadata({ params, searchParams }) {
   const resolvedSearchParams = await searchParams;
 
   const testId = resolvedParams.id;
-  const shareCode = resolvedSearchParams.share;
 
   let metadata = {};
 
   try {
     const assessmentResponse = await getAssessmentById(testId);
     const testName = assessmentResponse.data?.data?.name || "Тестийн үр дүн";
-    const icon = assessmentResponse.data?.icons;
+    const icon = assessmentResponse.data?.data?.icons;
 
-    if (shareCode) {
-      metadata = {
-        ...metadata,
-        title: `${testName} / Hire.mn`,
-        description: `Миний "${testName}" тестийн үр дүн`,
-        openGraph: {
-          ...metadata.openGraph,
-          title: `${testName} / Hire.mn`,
-          description: `Миний "${testName}" тестийн үр дүн`,
-          url: `https://hire.mn/test/${testId}`,
-          type: "website",
-          images: [
-            {
-              url: `https://hire.mn/api/share/${shareCode}`,
-              width: 1600,
-              height: 837.7,
-              alt: "Test Results",
-            },
-          ],
-        },
-      };
-    } else {
-      metadata = {
+    metadata = {
+      title: `${testName} / Hire.mn`,
+      description: "Онлайн тест, хөндлөнгийн үнэлгээ",
+      openGraph: {
         title: `${testName} / Hire.mn`,
         description: "Онлайн тест, хөндлөнгийн үнэлгээ",
-        openGraph: {
-          title: `${testName} / Hire.mn`,
-          description: "Онлайн тест, хөндлөнгийн үнэлгээ",
-          url: `https://hire.mn/test/${testId}`,
-          siteName: "Hire.mn",
-          type: "website",
-          images: [
-            {
-              url: icon ? `${api}file/${icon}` : "https://hire.mn/misc.png",
-              width: 1200,
-              height: 630,
-            },
-          ],
-        },
-        twitter: {
-          card: "summary_large_image",
-        },
-      };
-    }
+        url: `https://hire.mn/test/${testId}`,
+        siteName: "Hire.mn",
+        type: "website",
+        images: [
+          {
+            url: icon ? `${api}file/${icon}` : "https://hire.mn/misc.png",
+            width: 1200,
+            height: 630,
+          },
+        ],
+      },
+      twitter: {
+        card: "summary_large_image",
+      },
+    };
   } catch (error) {
     console.error("Error generating share metadata:", error);
   }
