c6121e46c4979ebb66698104393a42cf95a7e25a
usukhbaya12 <b20fa1714@ufe.edu.mn>
2025-05-12 13:33:03 +0800

    a

--- app/utils/routes.js
+++ app/utils/routes.js
@@ -1,2 +1,2 @@
-// export const api = "https://srv666826.hstgr.cloud/api/v1/";
-export const api = "http://localhost:3000/api/v1/";
+export const api = "https://srv666826.hstgr.cloud/api/v1/";
+// export const api = "http://localhost:3000/api/v1/";
